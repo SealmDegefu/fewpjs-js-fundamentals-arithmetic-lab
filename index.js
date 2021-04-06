4c927ac65cacf66f2a9be5d2d19990718c281355
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-06 09:42:46 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31,
+num2= 2,
+multiply = num1 * num2 
 
-// Write your code here
+function multiply(num1, num2){
+    num1 * num2 === 62
+  }
+
+let random = (Math.floor(Math.random() * 10) + 1)
+
+let num3 = 18,
+ num4 = 7,
+mod = (num3 % num4)
+
+ let max = Math.max(1, 2, 4, 20)
+
